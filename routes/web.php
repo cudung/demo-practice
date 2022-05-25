0eab854be793d3b204b68015c791d41c78ac2441
cudung <hoquangdung10062014@gmail.com>
2022-05-25 16:25:24 +0700

    change view welcome

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
 
 Route::get('/login/facebook', [SocialController::class, 'login']);
